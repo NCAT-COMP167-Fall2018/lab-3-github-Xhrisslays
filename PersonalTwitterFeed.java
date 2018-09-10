d2231f78da631e64e92b7ff5a1b3abc34bad3cca
Christopher Charles Brown <ccbrown4@ncat.edu>
2018-09-10 17:42:54 -0400

    new feature 2

---
+++ PersonalTwitterFeed.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalwitterfeed;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ccbrown4
+ */
+public class PersonalTwitterFeed {
+
+    private static final int MAX_NUMBER_TWEETS = 200;
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+String[] tweets = new String[MAX_NUMBER_TWEETS];
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to your personal Twitter!");
+        System.out.println("What's your name, tweeter?");
+        
+        String tweeterName = keyboard.nextLine();
+        
+        System.out.println("Nice to meet you " + tweeterName + "!");
+        System.out.println("Enter your tweets and I will add them to your timeline!");
+        
+        int numTweets = 0;
+        
+        while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
+            tweets[numTweets] = keyboard.nextLine() + getTimeStamp();
+            numTweets++;
+            
+            System.out.println(tweeterName + "'s Personal Twitter Feed:");
+            for(int i = 0; i < numTweets; i++) {
+                System.out.println("- " + tweets[i]);
+            }
+            
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            
+            if(numTweets < (MAX_NUMBER_TWEETS - 1))
+                System.out.println("Enter your next tweet:");
+        }
+        
+        System.out.println("Your twitter feed is full");
+    }
+    
+    public static String getTimeStamp(){
+        Date now = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
+        return dateFormat.format(now);
+    }
+    
+}
+    
+    
+
